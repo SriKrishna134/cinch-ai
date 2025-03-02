a1cd2b1f5cc2f49d14c147a9c699dfc51b03cae7
Srikrishna Hireholi <145562895+SriKrishna134@users.noreply.github.com>
2025-03-02 16:50:40 +0530

    Update ContactPage.jsx

--- cinch-main/frontend/src/components/ContactPage/ContactPage.jsx
+++ cinch-main/frontend/src/components/ContactPage/ContactPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabaseUrl, supabaseAnonKey, googleSheetUrl } from "../../constants";
+import { supabaseUrl, supabaseAnonKey } from "../../constants";
 import { createClient } from "@supabase/supabase-js";
 import "./ContactPage.css";
 import contactImage from "../../assets/contact.png"
